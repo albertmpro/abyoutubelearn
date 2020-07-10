87b0e70107b7e2ec1674c2ae80bc6f215e0afdba
Albert Michael Byrd <alxp2083@hotmail.com>
2020-07-10 18:33:36 -0400

    MyFirstApp

---
+++ AB_FirstProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace AB_FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is my string"; 
+            int myint = 37;
+            WriteLine("This is my code");
+            WriteLine("-----------------");
+            WriteLine(str);
+            WriteLine($"My age is {myint}");
+            ReadLine();
+          
+           
+        }
+    }
+}
