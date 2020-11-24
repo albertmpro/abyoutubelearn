90d1cf898bc85c5c7118275ce69205cb96e825d4
Albert Michael Byrd <alxp2083@hotmail.com>
2020-11-24 00:38:45 -0500

    String,Int,Double

---
+++ ABTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace ABTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Type 
+            string str = "Albert M. Byrd"; //Text 
+            int myint = 25; //Whole Numbers
+            double mydouble = 58.5; //Real Numbers 
+
+            WriteLine("ABTypes Program"); 
+            WriteLine("-------------------");
+            WriteLine($"My string = {str}");
+            WriteLine($"My int = {myint}");
+            WriteLine($"My double = {mydouble}");
+            ReadLine();
+
+
+        }
+    }
+}
