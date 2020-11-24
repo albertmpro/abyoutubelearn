d1afe4b1fe8e15cfdfed9eb531cf8955a214c6fd
Albert Michael Byrd <alxp2083@hotmail.com>
2020-11-24 13:35:42 -0500

    ScriptKidies Part 2

--- ABScriptKids/Program.cs
+++ ABScriptKids/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using static System.Console;
+//Tyoe your WriteLine Code heer...
+WriteLine("Script Kidies");
+ReadLine();
 
+/* THey way we've written console programs for 20 years
 namespace ABScriptKids
 {
     class Program
@@ -10,3 +15,4 @@ namespace ABScriptKids
         }
     }
 }
+*/
